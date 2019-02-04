2f76822dadff57f0e16e26a12405a7a086121e7d
Peter Kelly <peter.kelly@nginx.com>
2019-02-04 10:30:41 +0000

    Add basic structure

---
+++ pkg/builder/builder.go
@@ -0,0 +1,7 @@
+package builder
+
+// Build takes a string representing NGINX configuration
+// builds it into conf format and returns that as a string
+func Build(payload string, indent int, tabs, header bool) (string, error) {
+	return "built", nil
+}
