e001b8f99fdee20a8bfc4d0b59939f967391444e
Franz Blancaflor <franz.blancaflor@mycit.ie>
2019-02-28 15:52:42 +0000

    Fix brackets. Changed %s to %v.

--- pkg/builder/builder_test.go
+++ pkg/builder/builder_test.go
@@ -24,41 +24,43 @@ func TestBuild(t *testing.T) {
 func TestBuildNestedAndMultipleArgs(t *testing.T) {
 	var tests = []struct {
 		payload []Block
-	}{{
-		payload: []Block{
-			{
-				Directive: "events",
-				Args:      []string{" "},
-				Block: []Block{
-					Block{
-						Directive: "worker_connections",
-						Args:      []string{"1024"},
+	}{
+		{
+			payload: []Block{
+				{
+					Directive: "events",
+					Args:      []string{" "},
+					Block: []Block{
+						Block{
+							Directive: "worker_connections",
+							Args:      []string{"1024"},
+						},
 					},
 				},
-			},
-			{
-				Directive: "http",
-				Args:      []string{" "},
-				Block: []Block{
-					Block{
-						Directive: "server",
-						Args:      []string{" "},
-						Block: []Block{
-							Block{
-								Directive: "listen",
-								Args:      []string{"127.0.0.1:8080"},
-							},
-							Block{
-								Directive: "server_name",
-								Args:      []string{"default_server"},
-							},
-							Block{
-								Directive: "location",
-								Args:      []string{"/"},
-								Block: []Block{
-									Block{
-										Directive: "return",
-										Args:      []string{"200", "foo bar baz"},
+				{
+					Directive: "http",
+					Args:      []string{" "},
+					Block: []Block{
+						Block{
+							Directive: "server",
+							Args:      []string{" "},
+							Block: []Block{
+								Block{
+									Directive: "listen",
+									Args:      []string{"127.0.0.1:8080"},
+								},
+								Block{
+									Directive: "server_name",
+									Args:      []string{"default_server"},
+								},
+								Block{
+									Directive: "location",
+									Args:      []string{"/"},
+									Block: []Block{
+										Block{
+											Directive: "return",
+											Args:      []string{"200", "foo bar baz"},
+										},
 									},
 								},
 							},
@@ -67,13 +69,12 @@ func TestBuildNestedAndMultipleArgs(t *testing.T) {
 				},
 			},
 		},
-	},
 	}
 
 	for _, test := range tests {
 		out, err := json.Marshal(test)
 		if err != nil {
-			t.Errorf("Error %s", err.Error())
+			t.Errorf("Error %v", err)
 		}
 
 		c, err := Build(string(out), 4, false, false)
@@ -90,79 +91,81 @@ func TestBuildNestedAndMultipleArgs(t *testing.T) {
 func TestBuildWithComments(t *testing.T) {
 	var tests = []struct {
 		payload []Block
-	}{{
-		payload: []Block{
-			{
-				Directive: "events",
-				Line:      1,
-				Args:      []string{" "},
-				Block: []Block{
-					Block{
-						Directive: "worker_connections",
-						Line:      2,
-						Args:      []string{"1024"},
+	}{
+		{
+			payload: []Block{
+				{
+					Directive: "events",
+					Line:      1,
+					Args:      []string{" "},
+					Block: []Block{
+						Block{
+							Directive: "worker_connections",
+							Line:      2,
+							Args:      []string{"1024"},
+						},
 					},
 				},
-			},
-			{
-				Directive: "#",
-				Line:      4,
-				Args:      []string{" "},
-				Comment:   "comment",
-			},
-			{
-				Directive: "http",
-				Line:      5,
-				Args:      []string{" "},
-				Block: []Block{
-					Block{
-						Directive: "server",
-						Line:      6,
-						Args:      []string{" "},
-						Block: []Block{
-							Block{
-								Directive: "listen",
-								Line:      7,
-								Args:      []string{"127.0.0.1:8080"},
-							},
-							Block{
-								Directive: "#",
-								Line:      7,
-								Args:      []string{" "},
-								Comment:   "listen",
-							},
-							Block{
-								Directive: "server_name",
-								Line:      8,
-								Args:      []string{"default_server"},
-							},
-							Block{
-								Directive: "location",
-								Line:      9,
-								Args:      []string{"/"},
-								Block: []Block{
-									Block{
-										Directive: "#",
-										Line:      9,
-										Args:      []string{" "},
-										Comment:   "# this is brace",
-									},
-									Block{
-										Directive: "#",
-										Line:      10,
-										Args:      []string{" "},
-										Comment:   " location /",
-									},
-									Block{
-										Directive: "#",
-										Line:      11,
-										Args:      []string{" "},
-										Comment:   " is here",
-									},
-									Block{
-										Directive: "return",
-										Line:      12,
-										Args:      []string{"200", "foo bar baz"},
+				{
+					Directive: "#",
+					Line:      4,
+					Args:      []string{" "},
+					Comment:   "comment",
+				},
+				{
+					Directive: "http",
+					Line:      5,
+					Args:      []string{" "},
+					Block: []Block{
+						Block{
+							Directive: "server",
+							Line:      6,
+							Args:      []string{" "},
+							Block: []Block{
+								Block{
+									Directive: "listen",
+									Line:      7,
+									Args:      []string{"127.0.0.1:8080"},
+								},
+								Block{
+									Directive: "#",
+									Line:      7,
+									Args:      []string{" "},
+									Comment:   "listen",
+								},
+								Block{
+									Directive: "server_name",
+									Line:      8,
+									Args:      []string{"default_server"},
+								},
+								Block{
+									Directive: "location",
+									Line:      9,
+									Args:      []string{"/"},
+									Block: []Block{
+										Block{
+											Directive: "#",
+											Line:      9,
+											Args:      []string{" "},
+											Comment:   "# this is brace",
+										},
+										Block{
+											Directive: "#",
+											Line:      10,
+											Args:      []string{" "},
+											Comment:   " location /",
+										},
+										Block{
+											Directive: "#",
+											Line:      11,
+											Args:      []string{" "},
+											Comment:   " is here",
+										},
+										Block{
+											Directive: "return",
+											Line:      12,
+											Args:      []string{"200", "foo bar baz"},
+										},
 									},
 								},
 							},
@@ -171,13 +174,12 @@ func TestBuildWithComments(t *testing.T) {
 				},
 			},
 		},
-	},
 	}
 
 	for _, test := range tests {
 		out, err := json.Marshal(test)
 		if err != nil {
-			t.Errorf("Error %s", err.Error())
+			t.Errorf("Error %v", err)
 		}
 
 		c, err := Build(string(out), 4, false, false)
@@ -193,26 +195,27 @@ func TestBuildWithComments(t *testing.T) {
 func TestBuildStartsWithComments(t *testing.T) {
 	var tests = []struct {
 		payload []Block
-	}{{
-		payload: []Block{
-			{
-				Directive: "#",
-				Line:      1,
-				Args:      []string{" "},
-				Comment:   " foo",
-			},
-			{
-				Directive: "user",
-				Line:      5,
-				Args:      []string{"root"},
+	}{
+		{
+			payload: []Block{
+				{
+					Directive: "#",
+					Line:      1,
+					Args:      []string{" "},
+					Comment:   " foo",
+				},
+				{
+					Directive: "user",
+					Line:      5,
+					Args:      []string{"root"},
+				},
 			},
 		},
-	},
 	}
 	for _, test := range tests {
 		out, err := json.Marshal(test)
 		if err != nil {
-			t.Errorf("Error %s", err.Error())
+			t.Errorf("Error %v", err)
 		}
 
 		c, err := Build(string(out), 4, false, false)
@@ -228,20 +231,21 @@ func TestBuildStartsWithComments(t *testing.T) {
 func TestBuildWithQuotedUnicode(t *testing.T) {
 	var tests = []struct {
 		payload []Block
-	}{{
-		payload: []Block{
-			{
-				Directive: "env",
-				Line:      1,
-				Args:      []string{"русский текст"},
+	}{
+		{
+			payload: []Block{
+				{
+					Directive: "env",
+					Line:      1,
+					Args:      []string{"русский текст"},
+				},
 			},
 		},
-	},
 	}
 	for _, test := range tests {
 		out, err := json.Marshal(test)
 		if err != nil {
-			t.Errorf("Error %s", err.Error())
+			t.Errorf("Error %v", err)
 		}
 
 		c, err := Build(string(out), 4, false, false)
@@ -257,25 +261,26 @@ func TestBuildWithQuotedUnicode(t *testing.T) {
 func TestBuildFilesWithMissingStatusAndErrors(t *testing.T) {
 	var tests = []struct {
 		payload []ConfFiles
-	}{{
-		payload: []ConfFiles{
-			{
-				File: "nginx.conf",
-				Parsed: []Block{
-					Block{
-						Directive: "user",
-						Line:      1,
-						Args:      []string{"nginx"},
+	}{
+		{
+			payload: []ConfFiles{
+				{
+					File: "nginx.conf",
+					Parsed: []Block{
+						Block{
+							Directive: "user",
+							Line:      1,
+							Args:      []string{"nginx"},
+						},
 					},
 				},
 			},
 		},
-	},
 	}
 	for _, test := range tests {
 		out, err := json.Marshal(test)
 		if err != nil {
-			t.Errorf("Error %s", err.Error())
+			t.Errorf("Error %v", err)
 		}
 
 		c, err := BuildFiles(string(out), "none", 4, false, false)
@@ -318,7 +323,7 @@ func TestBuildFilesWithUnicode(t *testing.T) {
 	for _, test := range tests {
 		out, err := json.Marshal(test)
 		if err != nil {
-			t.Errorf("Error %s", err.Error())
+			t.Errorf("Error %v", err)
 		}
 
 		c, err := BuildFiles(string(out), "none", 4, false, false)
