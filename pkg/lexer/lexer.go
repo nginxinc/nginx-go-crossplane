883c589a2a69083faab2d6c324117f3133d9e639
Seth Malaki <seth.malaki@nginx.com>
2019-03-03 07:55:19 +0000

    Basic lexer tests - TestBalanceBraces expects an array of lexemes to have balanced curly braces - TestLexBasic covers 80% of basic nginx.conf grammar and a messy test case

--- pkg/lexer/lexer.go
+++ pkg/lexer/lexer.go
@@ -1,1 +1,21 @@
 package lexer
+
+// UnbalancedBracesError -
+type UnbalancedBracesError string
+
+// LexicalItem -
+type LexicalItem struct {
+	item    string
+	lineNum int
+}
+
+// balance braces found in a lexical item array.
+// returns an error if lexemes right and left braces are not balanced
+func balanceBraces([]LexicalItem) UnbalancedBracesError {
+	return ""
+}
+
+// Lex - Generates tokens from an nginx config file
+func Lex(filename string) ([]LexicalItem, error) {
+	return []LexicalItem{}, nil
+}
