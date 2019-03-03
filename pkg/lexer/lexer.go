98875d74dc02740b069e64bbcf406cbde4099e76
Seth Malaki <seth.malaki@nginx.com>
2019-03-03 09:15:01 +0000

    INGDEV-440 Basic implementation of balanceBraces

--- pkg/lexer/lexer.go
+++ pkg/lexer/lexer.go
@@ -1,5 +1,7 @@
 package lexer
 
+import "sync"
+
 // UnbalancedBracesError -
 type UnbalancedBracesError string
 
@@ -11,8 +13,35 @@ type LexicalItem struct {
 
 // balance braces found in a lexical item array.
 // returns an error if lexemes right and left braces are not balanced
-func balanceBraces([]LexicalItem) UnbalancedBracesError {
+func balanceBraces(lexicalItems []LexicalItem) UnbalancedBracesError {
+	var (
+		mu      sync.Mutex
+		wg      sync.WaitGroup
+		balance int
+	)
+
+	for _, lexicalItem := range lexicalItems {
+		wg.Add(1)
+		go func(i LexicalItem) {
+			defer wg.Done()
+			mu.Lock()
+			switch i.item {
+			case "{":
+				balance = balance + 1
+				break
+			case "}":
+				balance = balance - 1
+				break
+			}
+			mu.Unlock()
+		}(lexicalItem)
+	}
+	wg.Wait()
+	if balance != 0 {
+		return "UnbalancedBracesError: braces are not balanced"
+	}
 	return ""
+
 }
 
 // Lex - Generates tokens from an nginx config file
