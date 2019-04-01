5d5c2eedb129f299792677153c64c1053a39d0c6
LorcanMcVeigh <lorcan.mcveigh@gmail.com>
2019-04-01 16:17:57 +0100

    added test for simple/nginx.conf

--- pkg/parser/parser_test.go
+++ pkg/parser/parser_test.go
@@ -1,36 +1,50 @@
 package parser
 
 import (
+	"encoding/json"
+	"path/filepath"
+	"reflect"
 	"testing"
 )
 
-/*
-func TestIncludes(t *testing.T) {
-	var includePayload = []struct {
-		payload []Config
-	}{
-		{
-			payload: []Config{
-				{
-					File:   "nginx.conf",
-					Status: "ok",
-					Parsed: []Block{
-						Block{
-							Directive: "include",
-							Line:      1,
-							Args:      []string{" "},
-						},
-					},
-				},
-			},
-		},
+func TestParsingSimple(t *testing.T) {
+
+	dir, err := filepath.Abs("configs/simple/nginx.conf")
+	if err != nil {
+		t.Error(err)
+	}
+	// making the test data
+	b := Block{directive: "worker_connections", line: 2, args: []string{"1024"}}
+	b1 := Block{directive: "return", line: 10, args: []string{"200", "foo bar baz"}}
+	b2 := Block{directive: "location", line: 9, args: []string{"/"}, block: []Block{b1}}
+	b3 := Block{directive: "server", line: 6, args: []string{}, block: []Block{b2}}
+	b4 := Block{directive: "http", line: 5, args: []string{}, block: []Block{b3}}
+	b5 := Block{directive: "events", line: 1, args: []string{}, block: []Block{b}}
+	c := Config{file: dir, status: "ok", errors: []Errors{}, parsed: []Block{b4, b5}}
+	e := Payload{status: "ok", errors: []string{}, conf: c}
+
+	var d1, d2 interface{}
+	payload, err := Parsing(dir)
+	if err != nil {
+		t.Error(err)
+	}
+	expectedData, err := json.Marshal(e)
+	if err := json.Unmarshal(payload, &d1); err != nil {
+		t.Error(err)
 	}
 
-}
-*/
+	if err := json.Unmarshal(expectedData, &d2); err != nil {
+		t.Error(err)
+	}
 
-func TestParsing(t *testing.T) {
-	var tests = []struct {
+	if res := reflect.DeepEqual(d2, d1); !res {
+		// they're not equal
+		t.Errorf("expected data does not match test json generated")
+	}
+
+	// not equal
+
+	/*var tests = []struct {
 		title    string
 		payload  []Config
 		expected ParsingError
@@ -58,13 +72,6 @@ func TestParsing(t *testing.T) {
 			},
 			"",
 		},
-	}
+	}*/
 
-	for _, test := range tests {
-		t.Log(test.title)
-		err := Parsing(test.payload)
-		if err != test.expected {
-			t.Errorf("Error: \t\nexpected: %v, \t\nactual: %v", test.expected, err)
-		}
-	}
 }
