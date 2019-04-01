c9783190df9e49a7106f71de47f50032bfef15a2
LorcanMcVeigh <lorcan.mcveigh@gmail.com>
2019-04-01 16:34:17 +0100

    parent 9bf440b57a0219d7f82c36b23c1da5856c3002f1 author LorcanMcVeigh <lorcan.mcveigh@gmail.com> 1554132857 +0100 committer LorcanMcVeigh <lorcan.mcveigh@gmail.com> 1554803653 +0100
    Add Parser

--- pkg/parser/parser.go
+++ pkg/parser/parser.go
@@ -1,13 +1,21 @@
 package parser
 
-import "encoding/json"
+import (
+	"fmt"
+)
+
+// LexicalItem -
+type LexicalItem struct {
+	item    string
+	lineNum int
+}
 
 // ParseArgs -
 type ParseArgs struct {
 	FileName string
-	// onerror     bool
+	//onerror
 	CatchErrors bool
-	Ignore      string
+	Ignore      []string
 	Single      bool
 	Comments    bool
 	Strict      bool
@@ -23,7 +31,7 @@ type ParsingError string
 type Config struct {
 	File   string
 	Status string
-	Errors []Errors
+	Errors []ParseErrors
 	Parsed []Block
 }
 
@@ -34,26 +42,178 @@ type Block struct {
 	Args      []string
 	Includes  []int
 	Block     []Block
+	File      string
+	Comment   string
 }
 
-//Errors -
-type Errors struct {
+//ParseErrors -
+type ParseErrors struct {
 	File  string
 	Line  int
 	Error string
 }
 
-// Parse -
-func Parse(args ParseArgs) (string, error) {
+/*
+   Parses an nginx config file and returns json payload
 
-	return "parse", nil
+   :param filename: string contianing the name of the config file to parse
+   :param catch_errors: bool; if False, parse stops after first error
+   :param ignore: list or slice of directives to exclude from the payload
+   :param combine: bool; if True, use includes to create a single config obj
+   :param single: bool; if True, including from other files doesn't happen
+   :param comments: bool; if True, including comments to json payload
+   :param strict: bool; if True, unrecognized directives raise errors
+   :param check_ctx: bool; if True, runs context analysis on directives
+   :param check_args: bool; if True, runs arg count analysis on directives
+   :returns: a payload that describes the parsed nginx config
+*/
+
+func parse(a ParseArgs) (Config, error) {
+	data := []LexicalItem{
+		{item: "events", lineNum: 1},
+		{item: "{", lineNum: 1},
+		{item: "worker_connections", lineNum: 2},
+		{item: "1024", lineNum: 2},
+		{item: ";", lineNum: 2},
+		{item: "}", lineNum: 3},
+		{item: "http", lineNum: 5},
+		{item: "{", lineNum: 5},
+		{item: "server", lineNum: 6},
+		{item: "{", lineNum: 6},
+		{item: "listen", lineNum: 7},
+		{item: "127.0.0.1:8080", lineNum: 7},
+		{item: "server_name", lineNum: 8},
+		{item: "default_server", lineNum: 8},
+		{item: "location", lineNum: 9},
+		{item: "/", lineNum: 9},
+		{item: "{", lineNum: 9},
+		{item: "return", lineNum: 10},
+		{item: "200", lineNum: 10},
+		{item: "foo bar baz", lineNum: 10},
+		{item: "}", lineNum: 11},
+		{item: "}", lineNum: 12},
+		{item: "}", lineNum: 13},
+	}
+	includes := map[string][]string{
+		a.FileName: []string{},
+	}
+	p := Config{
+		File:   "",
+		Status: "ok",
+		Errors: []ParseErrors{},
+		Parsed: []Block{},
+	}
+	for f, r := range includes {
+		p.File = f
+		v, _ := Parsing(data, a, r)
+		p.Parsed = v
+	}
+	return p, nil
 }
 
 // Parsing -
-func Parsing(config []Config) ParsingError {
-	err := json.Marshal(tests)
-	if err != nil {
-		return "ParsingError"
+func Parsing(parsing []LexicalItem, a ParseArgs, ctx []string) ([]Block, int) {
+	newb := Block{}
+	w := 0
+	for i, p := range parsing {
+		w = i
+		b := Block{
+			Directive: "",
+			Comment:   "",
+			Includes:  []int{},
+			Line:      p.lineNum,
+			File:      "",
+			Args:      []string{},
+			Block:     []Block{},
+		}
+		if a.Combine {
+			b.File = a.FileName
+		}
+
+		if s := isDirective(p.item); s {
+			b.Directive = p.item
+		} else if s = checkifParent(p.item); s {
+			b.Directive = p.item
+		}
+		q := []byte{'#'}
+
+		if q[0] == p.item[0] {
+			if a.Comments {
+				b = Block{
+					Directive: "#",
+					Comment:   string(p.item[1:]),
+					Args:      []string{},
+					Block:     []Block{},
+					File:      "",
+					Line:      p.lineNum,
+					Includes:  []int{},
+				}
+			}
+		}
+
+		args := []string{}
+		count := 0
+		if p.item != "{" && p.item != ";" && p.item != "}" && isDirective(p.item) {
+			count++
+			p = parsing[i+count]
+			args = append(args, p.item)
+
+		} else {
+			continue
+		}
+		b.Args = args
+
+		if p.item == "{" {
+			v, _ := Parsing(parsing[i+1:], a, ctx)
+			if isDirective(v[0].Directive) {
+				b.Block = v
+			}
+
+		} else if p.item == "}" {
+			// cna't do his part until analyze and parser code are on the same branch
+			/*stm := statement{
+				directive: b.Directive,
+				args:      b.Args,
+				line:      b.Line,
+			}
+			inner := analyzer.enterBlockCTX(stm, ctx)
+			b.Block, _ = Parsing(parsing, a, inner)*/
+			break
+		}
+
+		newb.Block = append(newb.Block, b)
+
 	}
-	return ""
+	fmt.Println("newb : ", newb.Block)
+	b := []Block{newb}
+	return b, w
+}
+
+func checkifParent(s string) bool {
+	fmt.Println(s)
+	if s == "http" || s == "server" || s == "location" || s == "events" {
+		return true
+	}
+	return false
+}
+
+func isDirective(s string) bool {
+	d := []string{
+		"try_files",
+		"return",
+		"root",
+		"listen",
+		"error_log",
+		"default_type",
+		"access_log",
+		"user",
+		"worker_connections",
+	}
+	fmt.Println(s)
+	for _, t := range d {
+		if t == s {
+			return true
+		}
+	}
+	return false
 }
--- pkg/parser/parser_test.go
+++ pkg/parser/parser_test.go
@@ -2,76 +2,409 @@ package parser
 
 import (
 	"encoding/json"
-	"path/filepath"
 	"reflect"
 	"testing"
 )
 
-func TestParsingSimple(t *testing.T) {
-
-	dir, err := filepath.Abs("configs/simple/nginx.conf")
-	if err != nil {
-		t.Error(err)
-	}
-	// making the test data
-	b := Block{directive: "worker_connections", line: 2, args: []string{"1024"}}
-	b1 := Block{directive: "return", line: 10, args: []string{"200", "foo bar baz"}}
-	b2 := Block{directive: "location", line: 9, args: []string{"/"}, block: []Block{b1}}
-	b3 := Block{directive: "server", line: 6, args: []string{}, block: []Block{b2}}
-	b4 := Block{directive: "http", line: 5, args: []string{}, block: []Block{b3}}
-	b5 := Block{directive: "events", line: 1, args: []string{}, block: []Block{b}}
-	c := Config{file: dir, status: "ok", errors: []Errors{}, parsed: []Block{b4, b5}}
-	e := Payload{status: "ok", errors: []string{}, conf: c}
-
-	var d1, d2 interface{}
-	payload, err := Parsing(dir)
-	if err != nil {
-		t.Error(err)
-	}
-	expectedData, err := json.Marshal(e)
-	if err := json.Unmarshal(payload, &d1); err != nil {
-		t.Error(err)
-	}
-
-	if err := json.Unmarshal(expectedData, &d2); err != nil {
-		t.Error(err)
-	}
-
-	if res := reflect.DeepEqual(d2, d1); !res {
-		// they're not equal
-		t.Errorf("expected data does not match test json generated")
-	}
-
-	// not equal
-
-	/*var tests = []struct {
+func TestParse(t *testing.T) {
+	var tests = []struct {
 		title    string
-		payload  []Config
-		expected ParsingError
+		filename string
+		parargs  ParseArgs
+		config   []Config
 	}{
 		{
-			"simple",
+
+			"basic : test Parse ",
+			"config/simple.conf",
+			ParseArgs{
+				FileName:    "config/simple.conf",
+				CatchErrors: false,
+				Ignore:      []string{},
+				Single:      false,
+				Strict:      false,
+				Combine:     false,
+			},
 			[]Config{
 				{
-					File:   "nginx.conf",
+					File:   "simple.conf",
 					Status: "ok",
+					Errors: []ParseErrors{},
 					Parsed: []Block{
-						Block{
-							Directive: "events",
+						{
+							Directive: "http",
 							Line:      1,
+							Args:      []string{},
+							Includes:  []int{},
+							File:      "",
+							Comment:   "",
 							Block: []Block{
-								Block{
-									Directive: "worker_connections",
+								{
+									Directive: "server",
 									Line:      2,
-									Args:      []string{"1024"},
+									Args:      []string{},
+									Includes:  []int{},
+									File:      "",
+									Comment:   "",
+									Block: []Block{
+										{
+											Directive: "listen",
+											Args:      []string{"120.0.0.1:8080"},
+											Line:      3,
+											Includes:  []int{},
+											File:      "",
+											Comment:   "",
+										},
+									},
 								},
 							},
 						},
 					},
 				},
 			},
-			"",
 		},
-	}*/
+		{
 
+			"basic : messy test",
+			"config/messy.conf",
+			ParseArgs{
+				FileName:    "config/messy.conf",
+				CatchErrors: false,
+				Ignore:      []string{},
+				Single:      false,
+				Strict:      false,
+				Combine:     false,
+			},
+			[]Config{
+				{
+					File:   "config/messy.conf",
+					Status: "ok",
+					Errors: []ParseErrors{},
+					Parsed: []Block{
+						{
+							Directive: "user",
+							Args:      []string{"nobody"},
+							Line:      1,
+							Includes:  []int{},
+							File:      "",
+							Comment:   "",
+							Block:     []Block{},
+						},
+						{
+							Directive: "",
+							Args:      []string{},
+							Line:      2,
+							Includes:  []int{},
+							File:      "",
+							Comment:   "hello\\n\\\\n\\\\\\n worlddd  \\#\\\\#\\\\\\# dfsf\\n \\\\n \\\\\\n ",
+							Block:     []Block{},
+						},
+						{
+							Directive: "events",
+							Args:      []string{"worker_connections", "2048"},
+							Line:      3,
+							Includes:  []int{},
+							File:      "",
+							Comment:   "",
+							Block:     []Block{},
+						},
+						{
+							Directive: "http",
+							Args:      []string{"{"},
+							Line:      5,
+							Comment:   "forteen",
+							Includes:  []int{},
+							File:      "",
+							Block: []Block{
+								{
+									Directive: "",
+									Args:      []string{},
+									Line:      6,
+									Comment:   "this is a comment",
+									Includes:  []int{},
+									File:      "",
+									Block:     []Block{},
+								},
+								{
+									Directive: "access_log",
+									Args:      []string{"off"},
+									Line:      7,
+									Comment:   "",
+									Includes:  []int{},
+									File:      "",
+									Block:     []Block{},
+								},
+								{
+									Directive: "default_type",
+									Args:      []string{"text/plain"},
+									Line:      7,
+									Comment:   "",
+									Includes:  []int{},
+									File:      "",
+									Block:     []Block{},
+								},
+								{
+									Directive: "error_log",
+									Args:      []string{"off"},
+									Line:      7,
+									Comment:   "",
+									Includes:  []int{},
+									File:      "",
+									Block:     []Block{},
+								},
+								{
+									Directive: "server",
+									Args:      []string{"{"},
+									Line:      8,
+									Comment:   "",
+									Includes:  []int{},
+									File:      "",
+									Block: []Block{
+										{
+											Directive: "listen",
+											Args:      []string{"8080"},
+											Line:      9,
+											Comment:   "",
+											Includes:  []int{},
+											File:      "",
+											Block:     []Block{},
+										},
+										{
+											Directive: "return",
+											Args:      []string{`200","Ser\" \' \' ver\\\\ \\ $server_addr:\\$server_port\\n\\nTime: $time_local\\n\\n`},
+											Line:      10,
+											Comment:   "",
+											Includes:  []int{},
+											File:      "",
+											Block:     []Block{},
+										},
+									},
+								},
+								{
+									Directive: "server",
+									Args:      []string{"{"},
+									Line:      12,
+									Comment:   "",
+									Includes:  []int{},
+									File:      "",
+									Block: []Block{
+										{
+											Directive: "listen",
+											Args:      []string{"8080"},
+											Comment:   "",
+											Line:      12,
+											Includes:  []int{},
+											File:      "",
+											Block:     []Block{},
+										},
+										{
+											Directive: "root",
+											Args:      []string{"/usr/share/nginx/html"},
+											Line:      13,
+											Comment:   "",
+											Includes:  []int{},
+											File:      "",
+											Block:     []Block{},
+										},
+										{
+											Directive: "location",
+											Args:      []string{"~", "/hello/world"},
+											Comment:   "",
+											Line:      14,
+											Includes:  []int{},
+											File:      "",
+											Block: []Block{
+												{
+													Directive: "return",
+													Args:      []string{"301", "status.html"},
+													Line:      14,
+													Comment:   "",
+													File:      "",
+													Includes:  []int{},
+													Block:     []Block{},
+												},
+											},
+										},
+										{
+											Directive: "location",
+											Args:      []string{"/foo"},
+											Line:      15,
+											Comment:   "",
+											Includes:  []int{},
+											File:      "",
+											Block:     []Block{},
+										},
+										{
+											Directive: "location",
+											Args:      []string{"/bar"},
+											Line:      15,
+											Comment:   "",
+											Includes:  []int{},
+											File:      "",
+											Block:     []Block{},
+										},
+										{
+											Directive: "location",
+											Args:      []string{"/\\{\\;\\}\\ #\\ ab"},
+											Line:      16,
+											Comment:   "hello",
+											Includes:  []int{},
+											File:      "",
+											Block:     []Block{},
+										},
+										{
+											Directive: "if",
+											Args:      []string{"$request_method", "=", "P\\{O\\)\\###\\;ST"},
+											Line:      17,
+											Comment:   "",
+											Includes:  []int{},
+											File:      "",
+											Block:     []Block{},
+										},
+										{
+											Directive: "location",
+											Args:      []string{"/status.html"},
+											Line:      18,
+											Comment:   "",
+											Includes:  []int{},
+											File:      "",
+											Block: []Block{
+												{
+													Directive: "try_files",
+													Args:      []string{"/abc/${uri} /abc/${uri}.html", "=404"},
+													Line:      19,
+													Comment:   "",
+													Includes:  []int{},
+													File:      "",
+													Block:     []Block{},
+												},
+											},
+										},
+
+										{
+											Directive: "location",
+											Args:      []string{"/sta;\n                    tus"},
+											Line:      20,
+											Comment:   "",
+											Includes:  []int{},
+											File:      "",
+											Block: []Block{
+												{
+													Directive: "return",
+													Args:      []string{"302", "/status.html"},
+													Line:      21,
+													Comment:   "",
+													Includes:  []int{},
+													File:      "",
+													Block:     []Block{},
+												},
+											},
+										},
+
+										{
+											Directive: "location",
+											Args:      []string{"/upstream_conf"},
+											Line:      23,
+											Comment:   "",
+											Includes:  []int{},
+											File:      "",
+											Block: []Block{
+												{
+													Directive: "return",
+													Args:      []string{"200", "/status.html"},
+													Line:      23,
+													Comment:   "",
+													Includes:  []int{},
+													File:      "",
+													Block:     []Block{},
+												},
+											},
+										},
+										{
+											Directive: "server",
+											Args:      []string{},
+											Line:      24,
+											Comment:   "",
+											Includes:  []int{},
+											File:      "",
+											Block:     []Block{},
+										},
+									},
+								},
+							},
+						},
+					},
+				},
+			},
+		},
+		{
+
+			"basic : testwith comments",
+			"config/withComments.conf",
+			ParseArgs{
+				FileName:    "config/withComments.conf",
+				CatchErrors: false,
+				Ignore:      []string{},
+				Single:      false,
+				Strict:      false,
+				Combine:     false,
+			},
+			[]Config{
+				{
+					File:   "config/withComments.conf",
+					Status: "ok",
+					Errors: []ParseErrors{},
+					Parsed: []Block{
+						{
+							Directive: "http",
+							Args:      []string{},
+							Line:      1,
+							Includes:  []int{},
+							File:      "",
+							Comment:   "",
+							Block: []Block{
+								{
+									Directive: "server",
+									Line:      2,
+									Args:      []string{},
+									Includes:  []int{},
+									File:      "",
+									Comment:   "",
+									Block: []Block{
+										{
+											Directive: "listen",
+											Args:      []string{"120.0.0.1:8080"},
+											Line:      3,
+											Comment:   "listen",
+											Includes:  []int{},
+											File:      "",
+											Block:     []Block{},
+										},
+									},
+								},
+							},
+						},
+					},
+				},
+			},
+		},
+	}
+
+	for _, tes := range tests {
+
+		gen, err := parse(tes.parargs)
+		if err != nil {
+			panic(err)
+		}
+		dat, err := json.Marshal(tes.config)
+		if err != nil {
+			panic(err)
+		}
+		isEqual := reflect.DeepEqual(dat, gen)
+		if !isEqual {
+			t.Errorf("%v Failed : Generated Data is not the same", tes.title)
+		}
+		// deep equals etcs
+
+	}
 }
