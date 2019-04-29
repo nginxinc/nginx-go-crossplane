fb4c23bb377406c4955a567d8f0d727f29b9a586
Franz Blancaflor <franz.blancaflor@mycit.ie>
2019-04-29 14:41:12 +0100

    WIP: Fixed if statements

--- pkg/builder/builder.go
+++ pkg/builder/builder.go
@@ -41,63 +41,41 @@ func Build(payload string, indent int, tabs, header bool) (string, error) {
 		return "", fmt.Errorf("error unmarshalling payload: %v", err)
 	}
 	var result string
-	result = BuildBlock(payload, data, 0, 0)
+	var output string
+	result = BuildBlock(output, data, 0, 0)
 
 	return result, nil
 }
 
 // BuildBlock -
 func BuildBlock(output string, block []Block, depth, lastline int) string {
-	/*
-		b := Block{
-			Directive: " ",
-			Line:      0,
-			Args:      []string{},
-			Includes:  []int{},
-			Block:     []Block{},
-			File:      " ",
-			Comment:   " ",
-		}*/
-
 	var built string
-
 	for _, stmt := range block {
-		directive := stmt.Directive
-		line := stmt.Line
-		if directive == "#" && line == lastline {
+		//directive := stmt.Directive
+		var line int
+		if stmt.Directive == "#" && line == lastline {
 			output += " #" + stmt.Comment
 			continue
-		} else if directive == "#" {
-			built = "#" + "" + stmt.Comment
-		}
-		if stmt.Args != nil {
-			built = stmt.Directive + " " + strings.Join(stmt.Args, "")
+		} else if stmt.Directive == "#" {
+			built = "#" + stmt.Comment
 		} else {
-			built = directive
-		}
-		if stmt.Block == nil {
-			built += ";"
-		} else {
-			built += " {"
-			built = BuildBlock(built, stmt.Block, depth+1, line)
-			built += "\n " + "}"
-
+			if stmt.Directive == "if" {
+				built = "if (" + " " + strings.Join(stmt.Args, "") + ")"
+			} else if stmt.Args != nil {
+				built = stmt.Directive + " " + strings.Join(stmt.Args, "")
+			} else {
+				built = stmt.Directive
+			}
+			if len(stmt.Block) <= 0 {
+				built += ";"
+			} else {
+				built += " {"
+				built = BuildBlock(built, stmt.Block, depth, line)
+				built += "\n }"
+			}
 			output += " " + built
 			lastline = line
 		}
-		/*
-			if stmt.Directive == "#" {
-				b = Block{
-					Directive: "#",
-					Line:      stmt.Line,
-					Args:      []string{},
-					Includes:  []int{},
-					Block:     []Block{},
-					File:      " ",
-					Comment:   stmt.Comment,
-				}
-			}
-		*/
 	}
 	return output
 }
--- pkg/builder/builder_test.go
+++ pkg/builder/builder_test.go
@@ -58,7 +58,7 @@ func TestBuilderUltraSimple(t *testing.T) {
 			http {
 				server {
 					listen 127.0.0.1:8080;
-					#listen
+					#listen;
 				}
 			}
 			`,
