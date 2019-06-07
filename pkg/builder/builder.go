af990b562ed3e7a72d61c92980d059420fe65ec2
FranzBlancaflor <43249642+FranzBlancaflor@users.noreply.github.com>
2019-06-07 11:24:32 +0100

    BuildBlock Implementation (#11)
    * Change tests to follow go project best practices
    
    * Add title to BuildFiles test
    
    * WIP: Added BuildBlock function
    
    * WIP: Fixed if statements
    
    * WIP: Syntax changes
    
    * WIP: Added code for indentations
    
    * WIP: Added another unit test
    
    * WIP: Removed rogue spaces in BuildBlock and expected data
    
    * WIP: Made padding a global variable. Fixed expected outcome in tests
    
    * WIP: Added extra tests for nested and fixed if statement
    
    * WIP: Fixed formatting and removed commented code
    
    * WIP: Added test for start with comment
    
    * WIP: Removed unnecessary code in test
    
    * WIP: Removed unnecessary codes in builder.go
    
    * WIP: Resolve comment in PR
    
    * WIP: Resolved comment from PR
    
    * WIP: Added BuildFiles func and TestBuildFiles
    
    * WIP: Added missing code in BuildFiles
    
    * WIP: Remove unnecessary code in TestBuildFile
    
    * WIP: BuildFiles creates a directory within builder
    
    * WIP: BuildFiles able to make directory and add file
    
    * WIP: Removed trailing space in .conf file
    
    * WIP: Added another test for BuildFiles func
    
    * WIP: Changed variables and added ioutil.ReadFile so func can return a string
    
    * WIP: Added an expected outcome for TestBuilderFiles

--- pkg/builder/builder.go
+++ pkg/builder/builder.go
@@ -3,6 +3,11 @@ package builder
 import (
 	"encoding/json"
 	"fmt"
+	"io"
+	"io/ioutil"
+	"os"
+	"path/filepath"
+	"strings"
 )
 
 // Block -
@@ -10,38 +15,148 @@ type Block struct {
 	Directive string
 	Line      int
 	Args      []string
-	Comment   string
+	Includes  []int
 	Block     []Block
+	File      string
+	Comment   string
 }
 
-// ConfFiles -
-type ConfFiles struct {
+// Config -
+type Config struct {
 	File   string
 	Status string
-	Errors string
-	Config []ConfFiles
+	Errors []ParseError
 	Parsed []Block
 }
+
+// Payload -
+type Payload struct {
+	Status string
+	Errors []ParseError
+	File   string
+	Config []Config
+}
+
+// ParseError -
+type ParseError struct {
+	File  string
+	Line  string
+	Error string
+}
+
+var padding string
+var spacing int
 
 // Build takes a string representing NGINX configuration
 // builds it into conf format and returns that as a string
 func Build(payload string, indent int, tabs, header bool) (string, error) {
-	data := Block{}
+	data := []Block{}
 	err := json.Unmarshal([]byte(payload), &data)
 	if err != nil {
 		return "", fmt.Errorf("error unmarshalling payload: %v", err)
 	}
 
-	return "built", nil
+	if tabs {
+		padding = "\t"
+	} else {
+		padding = strings.Repeat(" ", indent)
+	}
+
+	spacing = indent
+	var b string
+
+	body := BuildBlock(b, data, 0, 0)
+
+	return body, nil
+}
+
+// BuildBlock -
+func BuildBlock(output string, block []Block, depth, lastline int) string {
+	var built string
+	margin := strings.Repeat(padding, depth)
+	tab := strings.Repeat("\t", spacing)
+
+	for _, stmt := range block {
+		line := 0
+
+		if stmt.Directive == "#" && line == lastline && stmt.Line != 1 {
+			output += " #" + stmt.Comment
+			continue
+		} else if stmt.Directive == "#" && stmt.Line == 1 {
+			output = "\n" + tab + "#" + stmt.Comment
+		} else {
+
+			if stmt.Directive == "if" {
+				built = "if (" + strings.Join(stmt.Args, " ") + ")"
+			} else if len(stmt.Args) > 0 {
+				built = stmt.Directive + " " + strings.Join(stmt.Args, " ")
+			} else {
+				built = stmt.Directive
+			}
+
+			if len(stmt.Block) < 1 {
+				built += ";"
+			} else {
+				built += " {"
+				built = BuildBlock(built, stmt.Block, depth+1, line)
+				built += "\n" + tab + margin + "}"
+			}
+
+			if output != " " {
+				output += "\n" + tab + margin + built
+			} else {
+				output += " " + margin + built
+			}
+			lastline = line
+			output = strings.Replace(output, "\t", padding, -1)
+		}
+	}
+	return output
 }
 
 // BuildFiles -
 func BuildFiles(payload string, dirname string, indent int, tabs, header bool) (string, error) {
-	data := ConfFiles{}
+	data := []Payload{}
 	err := json.Unmarshal([]byte(payload), &data)
+
 	if err != nil {
 		return "", fmt.Errorf("error unmarshalling payload: %v", err)
 	}
 
-	return "built", nil
+	var built string
+
+	if dirname == " " {
+		dirname, _ = os.Getwd()
+	}
+
+	for _, payload := range data {
+		for _, stmt := range payload.Config {
+			path := stmt.File
+			if !filepath.IsAbs(path) {
+				path = filepath.Join(dirname, path)
+			}
+
+			parts := strings.Split(stmt.File, "/")
+			dirpath := parts[0]
+			if _, err := os.Stat(dirpath); os.IsNotExist(err) {
+				os.Mkdir(dirpath, 0777)
+			}
+
+			parsed := stmt.Parsed
+			out, _ := json.Marshal(parsed)
+
+			output, _ := Build(string(out), 4, false, false)
+			output = strings.TrimLeft(output, "\n")
+
+			f, _ := os.Create(path)
+			_, err := io.WriteString(f, output)
+			if err != nil {
+				panic("No Output")
+			}
+
+			b, _ := ioutil.ReadFile(path)
+			built = string(b)
+		}
+	}
+	return built, nil
 }
