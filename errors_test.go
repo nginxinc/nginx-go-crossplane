ff8b7428284fa84fa399df031732561be97c7474
Valyria McFarland <v.mcfarland@f5.com>
2021-09-29 10:39:38 -0700

    fix: update premature end of file to be a ParseError

--- errors_test.go
+++ errors_test.go
@@ -10,23 +10,25 @@ func TestErrorString(t *testing.T) {
 	t.Parallel()
 	tf := "test.conf"
 	tl := 1
+	var te error
 
 	tcs := []struct {
 		f *string
 		w string
 		l *int
+		e error
 
 		exp string
 	}{
-		{&tf, "error", &tl, "error in test.conf:1"},
-		{nil, "error", &tl, "error in (nofile):1"},
-		{nil, "error", nil, "error in (nofile)"},
-		{&tf, "error", nil, "error in test.conf"},
-		{nil, "", nil, " in (nofile)"},
+		{&tf, "error", &tl, te, "error in test.conf:1"},
+		{nil, "error", &tl, te, "error in (nofile):1"},
+		{nil, "error", nil, te, "error in (nofile)"},
+		{&tf, "error", nil, te, "error in test.conf"},
+		{nil, "", nil, te, " in (nofile)"},
 	}
 
 	for _, tc := range tcs {
-		e := &ParseError{File: tc.f, Line: tc.l, What: tc.w}
+		e := &ParseError{File: tc.f, Line: tc.l, What: tc.w, originalErr: tc.e}
 
 		assert.Equal(t, tc.exp, e.Error())
 	}
