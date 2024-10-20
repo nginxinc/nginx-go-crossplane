daf3cc44c9eb34ad57dac9ac9f42477252228b9b
Matthew Mueller <mattmuelle@gmail.com>
2024-10-19 20:42:55 -0500

    package tools => package crossplane

--- tools.go
+++ tools.go
@@ -4,7 +4,7 @@
 // This file just exists to ensure we download the tools we need for building
 // See https://github.com/golang/go/wiki/Modules#how-can-i-track-tool-dependencies-for-a-module
 
-package tools
+package crossplane
 
 import (
 	_ "github.com/jstemmer/go-junit-report"
